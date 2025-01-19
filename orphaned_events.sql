94337c76b5d58dc21640aa43fd2be8c802e5453b
joshberickson <josherickson2@gmail.com>
2025-01-19 10:15:19 -0800

    Update orphaned_events.sql
    syntax

--- orphaned_events.sql
+++ orphaned_events.sql
@@ -2,32 +2,32 @@
 
 with created as (
 select
-    'created' as type
-  , count(distinct c.event_id) as events
-  , count(distinct case when s.user_id is null then c.event_id else null end) as disregard_events
+    'created' as type,
+    count(distinct c.event_id) as events,
+    count(distinct case when s.user_id is null then c.event_id else null end) as disregard_events
 from dbt_dev.dbt_jerickson.created c
 left join dbt_dev.dbt_jerickson.selected s
-  on c.user_id = s.user_id
-  and c.institution_name = s.institution_name
-  and c.data_provider = s.data_provider
-  and s.type = 'connect'
-  and to_timestamp(c.timestamp, 'M/d/yy H:mm') > to_timestamp(s.timestamp, 'M/d/yy H:mm')
+    on c.user_id = s.user_id
+    and c.institution_name = s.institution_name
+    and c.data_provider = s.data_provider
+    and s.type = 'connect'
+    and to_timestamp(c.timestamp, 'M/d/yy H:mm') > to_timestamp(s.timestamp, 'M/d/yy H:mm')
 where 1=1
-  and c.type = 'created'
+    and c.type = 'created'
 group by 1
-)
-, disconnected as (
+),
+disconnected as (
 select
-    'disconnected' as type
-  , count(distinct d.event_id) as events
-  , count(distinct case when c.credential_id is null then d.event_id else null end) as disregard_events
+    'disconnected' as type,
+    count(distinct d.event_id) as events,
+    count(distinct case when c.credential_id is null then d.event_id else null end) as disregard_events
 from dbt_dev.dbt_jerickson.disconnected d
 left join dbt_dev.dbt_jerickson.created c
-  on d.credential_id = c.credential_id
-  and c.type = 'created'
-  and to_timestamp(d.timestamp, 'M/d/yy H:mm') > to_timestamp(c.timestamp, 'M/d/yy H:mm')
+    on d.credential_id = c.credential_id
+    and c.type = 'created'
+    and to_timestamp(d.timestamp, 'M/d/yy H:mm') > to_timestamp(c.timestamp, 'M/d/yy H:mm')
 where 1=1
-  and d.type = 'disconnected'
+    and d.type = 'disconnected'
 group by 1
 )
 select *
@@ -35,7 +35,6 @@ from created c
 union all
 select *
 from disconnected d
-
 
 /*
 individual data checks for the data returned above
